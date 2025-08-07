5489f5b45fa1ccdc2690545f4a8a0b556caabe50
seans86 <sean.setoki@gmail.com>
2025-08-08 01:45:50 +0200

    fixed the subnets' cidrs pt10

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   public_subnet1_cidr       = var.public_subnet1_cidr
   public_subnet2_cidr       = var.public_subnet2_cidr
   private_subnet1_cidr      = var.private_subnet1_cidr
-  private_subnet2_cidr      = var.public_subnet2_cidr
+  private_subnet2_cidr      = var.private_subnet2_cidr
   availability_zone1        = var.availability_zone1
   availability_zone2        = var.availability_zone2
 
