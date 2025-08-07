fefd543d3911ab9d0427c642bbb6dfcea4dafdea
seans86 <sean.setoki@gmail.com>
2025-08-07 12:22:21 +0200

    Created the vpc module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+
+terraform
+variable "vpc_cidr" {
+type = string
+}
+
+variable "public_subnet1_cidr" {
+type = string
+}
+
+variable "public_subnet2_cidr" {
+type = string
+}
+
+variable "private_subnet1_cidr" {
+type = string
+}
+
+variable "private_subnet2_cidr" {
+type = string
+}
+
+variable "availability_zone1" {
+type = string
+}
+
+variable "availability_zone2" {
+type = string
+}
