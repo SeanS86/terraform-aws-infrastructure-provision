ee588d70fd7365aa33aa2e73a09be71b55baefc7
seans86 <sean.setoki@gmail.com>
2025-08-07 12:34:19 +0200

    Created the load_balancer module

---
+++ modules/load_balancer/variables.tf
@@ -0,0 +1,19 @@
+variable "public_subnet_id1" {
+  type = string
+}
+
+variable "public_subnet_id2" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "k8s_node1_id" {
+  type = string
+}
+
+variable "k8s_node2_id" {
+  type = string
+}
